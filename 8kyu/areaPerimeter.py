6cc22daccf340f9d30df514c1b0a745ea269e543
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-07-29 22:58:34 +0800

    areaPerimter

---
+++ 8kyu/areaPerimeter.py
@@ -0,0 +1,21 @@
+# You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+# If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+# Example(Input1, Input2 --> Output):
+
+# 6, 10 --> 32
+# 3, 3 --> 9
+
+def area_or_perimeter(l , w):
+    
+    if l == w:
+        return l*w
+    else: 
+        return 2*(l+w)
+    
+
+#my clever one liner solution
+
+def area_or_perimeter(l , w):
+    
+    return l*w if l == w else 2*(l+w)
