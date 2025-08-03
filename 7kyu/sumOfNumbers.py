6f54c5ad97db58360903fc87eba6b0384e8ef4ec
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-08-03 23:52:35 +0800

    sumOfNumbers

---
+++ 7kyu/sumOfNumbers.py
@@ -0,0 +1,27 @@
+# Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+# Note: a and b are not ordered!
+
+# Examples (a, b) --> output (explanation)
+# (1, 0) --> 1 (1 + 0 = 1)
+# (1, 2) --> 3 (1 + 2 = 3)
+# (0, 1) --> 1 (0 + 1 = 1)
+# (1, 1) --> 1 (1 since both are same)
+# (-1, 0) --> -1 (-1 + 0 = -1)
+# (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+def get_sum(a,b):
+    
+    sum = 0
+    
+    if a < b:
+        for n in range(a, b+1):
+            sum += n
+    
+    else:
+        for n in range(b, a+1):
+            sum += n
+    
+    return sum
+
+print(get_sum(0,-1))
