e84c8a01ac9e8682736ca5d0cf91b2701ea238ed
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-08-04 14:19:08 +0800

    countsheep

---
+++ 8kyu/countsheep.py
@@ -0,0 +1,15 @@
+# If you can't sleep, just count sheeps!!
+
+# Task:
+# Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+def count_sheep(n):
+    
+    list = []
+    
+    for x in range(0, n):
+        list.append(str(x+1)+ " sheep...")
+    
+    return "".join(list)
+
+print(count_sheep(2))
