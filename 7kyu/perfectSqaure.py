380063dc96b7c641ff708aa1cc4324c6f7b13ce8
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-08-05 12:49:06 +0800

    nextperfectsqaure

---
+++ 7kyu/perfectSqaure.py
@@ -0,0 +1,18 @@
+# You might know some pretty large perfect squares. But what about the NEXT one?
+
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+# If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+# Examples ( Input --> Output )
+# 121 --> 144
+# 625 --> 676
+# 114 --> -1  #  because 114 is not a perfect square
+
+def find_next_square(sq):
+    
+    nextroot = (sq ** (1/2)) + 1
+
+    return nextroot * nextroot if (sq ** (1/2))%1 == 0 else -1
+
+print(find_next_square(121))
