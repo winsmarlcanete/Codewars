dc22138d1cc1fd450b51331e41bb1d3be64a9e8c
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-08-06 23:42:53 +0800

    string repeat

---
+++ 8kyu/stringrepeat.py
@@ -0,0 +1,9 @@
+# Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+def repeat_str(repeat, string):
+    
+    return "".join(string for x in range(repeat))
