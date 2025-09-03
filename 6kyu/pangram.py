2c47daddca5b72aedc321b84a8e5545d03e94fa9
Winsmarl <110825438+winsmarlcanete@users.noreply.github.com>
2025-09-03 23:34:57 +0800

    pangram

---
+++ 6kyu/pangram.py
@@ -0,0 +1,14 @@
+# A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+def is_pangram(st):
+
+    list = []
+    
+    for x in set(st.lower()):
+        
+        if x.isalpha():
+            list.append(x)
+            
+    return len(list) == 26
