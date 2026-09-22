b1cc5af4846b8590b7c44c3285e2d28f621869ba
Winsmarl P. Cañete <WinsmarlPC@dom2.eelhi.network>
2026-09-22 15:51:56 +0800

    weird string case

---
+++ 6kyu/weirdStringCase.php
@@ -0,0 +1,22 @@
+<!-- Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zeroth index is even, therefore that character should be upper cased. Indexing resets for each word. In other words, the first letter of every word is index 0 (even), so it must always be uppercase, etc.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+"String" => "StRiNg"
+"Weird string case" => "WeIrD StRiNg CaSe" -->
+
+<?php
+
+function toWeirdCase(string $string) : string {
+  $words = explode(" ", strtoupper($string));
+  $weirdWords;
+  foreach ($words as $word) {
+    for ($i = 1; $i < strlen($word); $i += 2) {
+      $word[$i] = strtolower($word[$i]);
+    }
+    $weirdWords[] = $word;
+  }
+  
+  return implode(" ", $weirdWords);
+}
